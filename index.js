40fa4a04765599491b78d94bb6a7ff388ab6b922
Ubuntu <ubuntu@ip-172-31-85-92.ec2.internal>
2020-03-11 17:55:27 +0000

    index added

---
+++ index.js
@@ -0,0 +1,10 @@
+// JavaScript File
+function backgroundColor() {
+  document.getElementById('bkcolor').style.backgroundColor="coral";
+}
+function funcName() {
+    alert("Hello, World.")
+}
+function functName() {
+    alert("I'm slowly getting the hang of JavaScript!")
+}
